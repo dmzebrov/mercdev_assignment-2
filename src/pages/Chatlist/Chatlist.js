3867aeede58e4a8b181a20480443ad63cd8681d1
dmzebrov <dmzebrov@gmail.com>
2019-10-18 19:40:37 +0400

    Default create-react-app reset. Ready for development

---
+++ src/pages/Chatlist/Chatlist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Chatlist.css';
+
+class Chatlist extends React.Component {
+  render() {
+    return (
+      <h1>Hello world</h1>
+    )
+  }
+}
+
+export default Chatlist;
