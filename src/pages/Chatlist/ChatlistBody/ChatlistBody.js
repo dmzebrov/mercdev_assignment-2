ef36e13db094fceeac1974992a035b3e4246dc17
dmzebrov <dmzebrov@gmail.com>
2019-10-20 01:47:36 +0400

    ChatlistBody component blank added

---
+++ src/pages/Chatlist/ChatlistBody/ChatlistBody.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ChatlistBody.css';
+import ChatlistHead from '../ChatlistHead/ChatlistHead';
+
+function ChatlistBody({ chatList, ...otherProps }) {
+  return (
+    <div className="chatlist-body">
+      <div className="chatlist-body__container">
+        <h1>hello world</h1>
+      </div>
+    </div>
+  )
+}
+
+export default ChatlistBody;
