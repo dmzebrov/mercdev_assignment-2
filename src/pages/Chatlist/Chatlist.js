586647308c228c33bec70812ef58701c63ba8e6d
dmzebrov <dmzebrov@gmail.com>
2019-10-20 01:44:33 +0400

    Chatlist page added

--- src/pages/Chatlist/Chatlist.js
+++ src/pages/Chatlist/Chatlist.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Chatlist.css';
 
+import ChatlistHead from './ChatlistHead/ChatlistHead';
+import ChatlistBody from './ChatlistBody/ChatlistBody';
+
 class Chatlist extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.searchChat = this.searchChat.bind(this);
+
+
+  }
+
+  searchChat() {
+    console.log('search');
+  }
+
   render() {
     return (
-      <h1>Hello world</h1>
+      <>
+        <ChatlistHead searchChat={this.searchChat} />
+
+        <ChatlistBody />
+      </>
     )
   }
 }
