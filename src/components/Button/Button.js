7ffa0db9e5ab36e4eed00c2ddab6e5197407f94e
Dima Zebrov <dmzebrov@gmail.com>
2019-11-04 16:34:23 +0400

    Boolean variable renamed, conditional class application refactored

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,16 +4,16 @@ import "./Button.css";
 import classNames from "classnames";
 
 function Button({ className, ...otherProps }) {
-  const touchsupport =
+  const isTouchEventSupported =
     "ontouchstart" in window ||
     navigator.maxTouchPoints > 0 ||
     navigator.msMaxTouchPoints > 0;
 
-  let buttonPCHoverClass = !touchsupport ? "button_button-hover" : "";
-
   return (
     <button
-      className={classNames("button", buttonPCHoverClass, className)}
+      className={classNames("button", className, {
+        "button_button-hover": isTouchEventSupported
+      })}
       {...otherProps}
     />
   );
